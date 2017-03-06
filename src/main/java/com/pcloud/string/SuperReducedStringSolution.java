fe56d4dc855473482665a8bfdaaecaa652ab29f7
The Tran <thetran@imacbook.local>
2017-03-07 06:00:48 +0700

    Hacker Rank: Super reduce String problem - Add description for problem

--- src/main/java/com/pcloud/string/SuperReducedStringSolution.java
+++ src/main/java/com/pcloud/string/SuperReducedStringSolution.java
@@ -1,7 +1,15 @@
 package com.pcloud.string;
 
 /**
- * Created by thetran on 3/7/17.
+ * Steve has a string, s, consisting of  lowercase English alphabetic letters.
+ * In one operation, he can delete any pair of adjacent letters with same value.
+ * For example, string "aabcc" would become either "aab" or "bcc" after operation.
+ *
+ * Steve wants to reduce  as much as possible.
+ * To do this, he will repeat the above operation as many times as it can be performed.
+ * Help Steve out by finding and printing 's non-reducible form!
+ *
+ * Note: If the final string is empty, print Empty String .
  */
 public class SuperReducedStringSolution {
     public static void main(String[] args) {
